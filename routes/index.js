10ea46a256b73f22b6b56623fc97c2c78cf06a86
CRFullStack <hobdy.chris02@gmail.com>
2020-10-30 15:14:59 -0400

    added a success page but failed to implement it.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var bodyParser = require("body-parser");
 const { readdirSync, rename } = require("fs");
 
 const helpers = require("../helpers/helpers");
-//var upload = multer({ dest: "./public/data/uploads/" });
 
 // Set Multer options here
 var storage = multer.diskStorage({
@@ -35,46 +34,16 @@ router.get("/about", function (req, res, next) {
   res.render("about");
 });
 
-router.post("/stats", upload.single("uploaded_file"), function (req, res) {
-  // req.file is the name of your file in the form above, here 'uploaded_file'
-  // req.body will hold the text fields, if there were any
-  //if (!req.file) {
-  //  return res.send("Please select an image to upload");
-  //}
-  storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/data/uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  //res.redirect(307, "/uploads");
-  console.log(storage);
-  console.log(req.file, req.body);
-});
-
-router.post("/preuploads", function (req, res) {
-  console.log(req.body);
-  storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/data/uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, req.body.newName + "_" + file.originalname);
-    },
-  });
-  upload = multer({ storage: storage, fileFilter: helpers.imageFilter });
-  res.redirect(307, "/uploads");
-});
+router.get("/success", function (req, res) {
+  res.render("success");
+});  
 
 router.post("/uploads", upload.array("uploaded_files", 10), async function (
   req,
   res
 ) {
   var dirPath = path.join(__dirname, "..\\public\\data\\uploads\\");
-  console.log(req.body);
-
+  
   // Get path to image directory
   const imageDirPath = dirPath;
 
@@ -89,7 +58,6 @@ router.post("/uploads", upload.array("uploaded_files", 10), async function (
       (err) => console.log(err)
     )
   );
-
   await res.zip({
     files: [
       {
@@ -99,6 +67,7 @@ router.post("/uploads", upload.array("uploaded_files", 10), async function (
     ],
     filename: "Package.zip",
   });
+
 });
 
 module.exports = router;
